15f95356d44bba60d37d78a6c3d30f183feedc90
kira9292 <abdoulkarimly008@gmail.com>
2024-10-08 16:17:23 +0000

    Initial commit

---
+++ src/main/java/com/example/backendnfc/BackendNfcApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendnfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNfcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendNfcApplication.class, args);
+    }
+
+}
